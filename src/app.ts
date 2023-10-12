cbd5c70e0d453ba852ad370969d7e31342879454
keylime7 <keylime7@cau.ac.kr>
2023-10-12 15:26:48 +0900

    feat: default routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
-import express, {Request, Response} from "express";
+import express from 'express';
+import indexRouter from './routes/index/index'
 
-const app = express()
+const app = express();
+const port = 8080;
 
-app.listen(8080)
+//미들웨어
+app.use(express.json());
+
+//라우터
+app.use('/', indexRouter);
+
+//서버 시작
+app.listen(port);
