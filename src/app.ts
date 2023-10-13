b76893fd1d74ebc694ee355a0a9aa8cd81495841
keylime7 <keylime7@cau.ac.kr>
2023-10-13 23:25:11 +0900

    feat: https setting

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,34 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import fs from 'fs';
+import https from "https";
 import indexRouter from './routes/index/index';
 
-if (process.env.NODE_ENV === 'production') {
-  dotenv.config({ path: './config/.env.production.local' });
-} else if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: './config/.env.development.local' });
-} else if (process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: './config/.env.test.local' });
+const environment: string = process.env.NODE_ENV || 'development';
+
+if (environment === 'production') {
+  dotenv.config({ path: 'src/config/.env.production.local' });
+} else if (environment === 'development') {
+  dotenv.config({ path: 'src/config/.env.development.local' });
+} else if (environment === 'test') {
+  dotenv.config({ path: 'src/config/.env.test.local' });
+}
+
+const privateKeyPath: string | undefined = process.env.PRIVATE_KEY_PATH;
+const certificatePath: string | undefined = process.env.CERTIFICATE_PATH;
+
+if (!privateKeyPath || !certificatePath) {
+  console.error('PRIVATE_KEY_PATH or CERTIFICATE_PATH not defined in .env file');
+  process.exit(1);
 }
 
 const app = express();
+const options = {
+  key: fs.readFileSync(privateKeyPath),
+  cert: fs.readFileSync(certificatePath)
+};
 const port = 8080;
+const server = https.createServer(options, app);
 
 //미들웨어
 app.use(express.json());
@@ -20,4 +37,4 @@ app.use(express.json());
 app.use('/', indexRouter);
 
 //서버 시작
-app.listen(port);
+server.listen(port);
