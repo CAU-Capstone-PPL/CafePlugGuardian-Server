f4c3194cbe731db0c11a1a36a440b4e757c49420
keylime7 <keylime7@cau.ac.kr>
2023-11-07 15:15:40 +0900

    cors middleware

--- src/config/expressApp.ts
+++ src/config/expressApp.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import apiRouter from '../routes/apiRoute';
 
 function configureExpressApp() {
@@ -6,6 +7,7 @@ function configureExpressApp() {
 
   //미들웨어
   app.use(express.json());
+  app.use(cors());
 
   //라우터
   app.use('/api', apiRouter);
