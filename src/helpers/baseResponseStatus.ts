a8ac1e76278d2a9e57786e0f851b26020c98580e
keylime7 <keylime7@cau.ac.kr>
2023-11-10 22:04:08 +0900

    feat: jwt 미들웨어

--- src/helpers/baseResponseStatus.ts
+++ src/helpers/baseResponseStatus.ts
@@ -6,6 +6,9 @@ const BaseResponseStatus: any = {
   //로그인
   LOGIN_SUCCESS: { message: '로그인 성공'},
   LOGIN_FAIL: { message: '아이디 혹은 비밀번호가 잘못 되었습니다.' },
+  SECRET_KEY_UNDEFINED: { message: "SECRET_KEY 환경 변수가 존재하지 않습니다." },
+  JWT_TOKEN_NULL: { message: "로그인 토큰이 존재하지 않습니다." },
+  JWT_FORBIDDEN: { message: "토큰이 유효하지 않습니다." }
 };
 
 export default BaseResponseStatus;
