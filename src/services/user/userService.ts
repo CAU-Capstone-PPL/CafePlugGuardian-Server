1f963053489d1a1e5a3c9f6d70f42d37240cb5ba
keylime7 <keylime7@cau.ac.kr>
2023-11-12 14:45:20 +0900

    feat: 로그인시 jwt 토큰 반환

--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -1,14 +1,29 @@
 import users from "../../models/users";
 import response from "../../helpers/response";
 import BaseResponseStatus from "../../helpers/baseResponseStatus";
+import jwt from "jsonwebtoken";
 
 class UserService {
   async verifyLogin(userId: string, userPw: string) {
     const user = await users.findOne({ userId: userId });
 
     if (user && user.userPw === userPw) {
+      const secretKey: string | undefined = process.env.SECRET_KEY;
+      if (secretKey == undefined) {
+        return response(BaseResponseStatus.SECRET_KEY_UNDEFINED);
+      }
+
+      const token = jwt.sign(
+        {
+          userId: user.userId,
+          userName: user.userName
+        },
+        secretKey,
+        { expiresIn: '1h' }
+      );
+
       return response(BaseResponseStatus.LOGIN_SUCCESS,
-        { 'Authorization': 'test' }
+        { 'Authorization': token }
       );
     } else {
       return response(BaseResponseStatus.LOGIN_FAIL);
