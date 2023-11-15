bd2b3e9a08af8cd83cccf9c61778d84c883a8d28
keylime7 <keylime7@cau.ac.kr>
2023-11-15 21:39:53 +0900

    feat: https에서 http로 변경

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,23 @@ import configureEnvironment from './config/environment';
 import connectDatabase from './config/database';
 import configureHTTPS from './config/https';
 import connectMQTTBroker from "./config/mqtt";
+import configureExpressApp from "./config/expressApp";
 
 configureEnvironment();
 connectDatabase();
 connectMQTTBroker();
 
+const PORT = process.env.PORT || 80;
+const server = configureExpressApp();
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+/* https 실행 코드
 const PORT = process.env.PORT || 443;
 const httpsServer = configureHTTPS();
 
 httpsServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+*/
